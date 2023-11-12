ead7b6422956b211accb05aaca8d145487d2865d
subham1265 <146015047+subham1265@users.noreply.github.com>
2023-11-12 14:16:01 +0530

    Add files via upload

---
+++ practice01.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+main()
+{
+	int a,b ;
+	printf("enter the value a :");
+	scanf("%d",&a);
+	
+	printf("enter the value b :");
+	scanf("%d",&b);
+	
+	int sum=a+b;
+	int diff=a-b;
+	int multi=a*b;
+	int div=a/b;
+	printf(" sum is :%d\n",sum);
+	printf(" diff is :%d\n",diff);
+	printf(" multi is :%d\n",multi);
+	printf(" div is :%d\n",div);
+	
+}  
